2cf4fad2936e76fea8d863a371bb586676866dfc
Zach Thompson <zach@allotropic.com>
2020-01-24 15:59:00 -0500

    Turn off WebRTC

--- etc/user.js
+++ etc/user.js
@@ -1,6 +1,9 @@
 // Tune up smooth-scrolling
-user_pref("general.smoothScroll.lines.durationMaxMS", 125);
-user_pref("general.smoothScroll.lines.durationMinMS", 125);
+//user_pref("general.smoothScroll.lines.durationMaxMS", 125);
+//user_pref("general.smoothScroll.lines.durationMinMS", 125);
+user_pref("general.smoothScroll.lines.durationMaxMS", 200);
+user_pref("general.smoothScroll.lines.durationMinMS", 100);
+
 user_pref("general.smoothScroll.mouseWheel.durationMaxMS", 200);
 user_pref("general.smoothScroll.mouseWheel.durationMinMS", 100);
 user_pref("general.smoothScroll.msdPhysics.enabled", true);
@@ -11,3 +14,6 @@ user_pref("general.smoothScroll.pages.durationMinMS", 125);
 
 // Allow Firefox to load chrome/userChrome.js
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Turn off WebRTC for secure VPN: https://mullvad.net/en/help/webrtc/
+user_pref("media.peerconnection.enabled", false);
